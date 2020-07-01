67984c1d324a3c691d6e605c93498e4fd6d90a15
Craig Jones <craig@k6nnl.com>
2020-07-01 04:26:27 -0700

    Some very minor refactoring.

--- utility/__init__.py
+++ utility/__init__.py
@@ -1,1 +1,5 @@
-# The existance of this file makes this subfolder a "package"
+# The existence of this file makes this subfolder a "package"
+"""
+This package contains general-purpose code that is not necessarily specific to this application.
+Don't be surprised if we move some/all of this code to an external library at some point.
+"""
