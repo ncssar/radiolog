4b8ee0b599fd4f3747e8980c157b0d6d15038b62
Craig Jones <craig@k6nnl.com>
2020-07-01 04:55:00 -0700

    Oops. Forgot to add config_helpers.py.

---
+++ utility/config_helpers.py
@@ -0,0 +1,8 @@
+import pathlib
+# import ast
+
+def asPath(input: str) -> pathlib.Path:
+    return pathlib.Path(input)
+
+# def asDict(input: str) -> Dict[str, str]:
+# 	return ast.literal_eval(input)
