0bcc711a70a4bb161a88108458987a17987e6936
Craig Jones <craig@k6nnl.com>
2020-08-02 12:16:44 -0700

    typo

--- utility/command_line.py
+++ utility/command_line.py
@@ -1,11 +1,10 @@
-
 class CommandLineSwitches:
 
 	def parse(self, args):
 		self.minMode=False
 		self.develMode=False
 		self.noSend=False
-		
+
 		if len(args)>1:
 			for arg in args[1:]:
 				if arg.lower()=="-devel":
@@ -18,4 +17,4 @@ class CommandLineSwitches:
 					self.noSend=True
 					print("Will not send any GET requests for this session.")
 				else:
-					print("ERROR Unrecognized command linbe switch: "+arg)
+					print("ERROR Unrecognized command line switch: "+arg)
