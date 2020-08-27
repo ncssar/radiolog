1c19ac36f838249aa805b0028d1efa8bbae4df15
Craig Jones <craig@k6nnl.com>
2020-08-26 23:21:37 -0700

    Standardized QLineEditWithDeselect.py. Synchronized the .gitignore between RadioLog and gwpycore.

--- QLineEditWithDeselect.py
+++ QLineEditWithDeselect.py
@@ -2,14 +2,15 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
+
 class QLineEditWithDeselect(QLineEdit):
-    def __init__(self,parent=None):
-        self.parent=parent
-        super(QLineEditWithDeselect,self).__init__(parent)
-    
+    def __init__(self, parent=None):
+        self.parent = parent
+        super(QLineEditWithDeselect, self).__init__(parent)
+
     # for some reason, the entire existing text (if any) was always selected;
     #  use this method to deselect all and move the cursor to the end
-    def focusInEvent(self,e):
-        QLineEdit.focusInEvent(self,e) # do this to show the blinking cursor
+    def focusInEvent(self, e):
+        QLineEdit.focusInEvent(self, e)  # do this to show the blinking cursor
         self.deselect()
         self.setCursorPosition(len(self.text()))
