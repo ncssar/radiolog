91c54375b54285e59bdb67c430a9f59dabcceba8
Tom Grundy <caver456@gmail.com>
2022-10-27 07:31:58 -0700

    WIP #506
    this fixes the original issue; will do another commit in this branch to use the same padding technique in message quick text as was just implemented for clue instructions quick text

--- radiolog.py
+++ radiolog.py
@@ -5181,6 +5181,7 @@ class newEntryWidget(QWidget,Ui_newEntryWidget):
 		self.dev=dev
 		self.parent=parent
 		self.isMostRecentForCallsign=isMostRecentForCallsign
+		self.quickTextJustAdded=0 # two-step counter used to insert space after quick text - see messageTextChanged
 		if amendFlag:
 			row=parent.radioLog[amendRow]
 			self.sec=row[6]
@@ -5402,6 +5403,7 @@ class newEntryWidget(QWidget,Ui_newEntryWidget):
 		else:
 			textToAdd="; "+quickText
 		self.quickTextAddedStack.append(textToAdd)
+		self.quickTextJustAdded=1
 		self.ui.messageField.setText(existingText+textToAdd)
 		self.ui.messageField.setFocus()
 
@@ -5809,6 +5811,17 @@ class newEntryWidget(QWidget,Ui_newEntryWidget):
 			
 	def messageTextChanged(self): # gets called after every keystroke or button press, so, should be fast
 ##		self.timer.start(newEntryDialogTimeoutSeconds*1000) # reset the timeout
+		#506 - if the most recent thing entered was a quick text, insert a space before the next thing typed;
+		#  since this function gets called by quick text too, we need to keep a counter so that the space
+		#  is insterted on the subsequent call to this function
+		if self.quickTextJustAdded>0: # don't  - add it on the next keystroke after quick text
+			if self.quickTextJustAdded==2:
+				# rprint('  the most recent action was a quick text - adding a space')
+				self.quickTextJustAdded=0
+				prev=self.ui.messageField.text()
+				self.ui.messageField.setText(prev[:-1]+' '+prev[-1])
+			else:
+				self.quickTextJustAdded+=1
 		message=self.ui.messageField.text().lower()
 		extTeamName=getExtTeamName(self.ui.teamField.text())
 		prevStatus=""
