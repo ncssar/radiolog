adec484490ee9d440ae0c4d13ffe5b4cd74e51da
Tom Grundy <caver456@gmail.com>
2023-12-27 08:45:15 -0800

    3.11.0

--- radiolog.py
+++ radiolog.py
@@ -333,7 +333,7 @@ from FingerTabs import *
 from pygeodesy import Datums,ellipsoidalBase,dms
 from difflib import SequenceMatcher
 
-__version__ = "3.10.2"
+__version__ = "3.11.0"
 
 # process command-line arguments
 develMode=False
